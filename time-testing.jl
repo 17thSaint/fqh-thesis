2b5909572a74a6fe9d81323831e714af205f96f1
17thSaint <pgeraghty3@outlook.com>
2022-04-19 12:02:32 +0200

    all time testing funcs, about to remove

--- time-testing.jl
+++ time-testing.jl
@@ -67,9 +67,11 @@ end
 #
 part_choices = [2,4,6,8]
 time_vals_ext = [0.0 for i in 1:length(part_choices)]
+time_vals_int = [0.0 for i in 1:length(part_choices)]
 time_vals_sing = [0.0 for i in 1:length(part_choices)]
 time_vals_column = [0.0 for i in 1:length(part_choices)]
 error_vals_ext = [0.0 for i in 1:length(part_choices)]
+error_vals_int = [0.0 for i in 1:length(part_choices)]
 error_vals_sing = [0.0 for i in 1:length(part_choices)]
 error_vals_column = [0.0 for i in 1:length(part_choices)]
 for i in 1:length(part_choices)
@@ -88,11 +90,13 @@ println(particles)
 time_vals_sing_local = [0.0 for j in 1:5]
 time_vals_ext_local = [0.0 for j in 1:5]
 time_vals_column_local = [0.0 for j in 1:5]
+time_vals_int_local = [0.0 for j in 1:5]
 #read_acc_matrix_data("acc-matrix-data",particles,1,1)
 #
 get_rf_wavefunc_indivelemcall(this_config,full_pasc_tri,full_derivs,[0,[0]],true)
 get_rf_wavefunc_columncall(this_config,full_pasc_tri,full_derivs,[0,[0]],true)
 get_rf_wavefunc(this_config,acc_mat,full_pasc_tri,full_derivs,[0,[0]],true)
+get_rf_wavefunc_internal(this_config,full_pasc_tri,full_derivs,[0,[0]],true)
 #
 for j in 1:5
 
@@ -108,14 +112,21 @@ time_start_ext = now()
 ext = get_rf_wavefunc(this_config,acc_mat,full_pasc_tri,full_derivs,[0,[0]],true)
 time_end_ext = now()
 time_vals_ext_local[j] = (time_end_ext - time_start_ext).value
+
+time_start_int = now()
+int = get_rf_wavefunc_internal(this_config,full_pasc_tri,full_derivs,[0,[0]],true)
+time_end_int = now()
+time_vals_int_local[j] = (time_end_int - time_start_int).value
 #
 time_start_column = now()
 col = get_rf_wavefunc_columncall(this_config,full_pasc_tri,full_derivs,[0,[0]],true)
 #read_acc_matrix_data("acc-matrix-data",particles,my_parts,1)
 time_end_column = now()
 time_vals_column_local[j] = (time_end_column - time_start_column).value
+
 @test isapprox(ind,ext,atol=sqrt(eps()))
 @test isapprox(ind,col,atol=sqrt(eps()))
+@test isapprox(int,col,atol=sqrt(eps()))
 end
 time_vals_sing[i] = mean(time_vals_sing_local)
 error_vals_sing[i] = std(time_vals_sing_local)
@@ -123,12 +134,15 @@ time_vals_ext[i] = mean(time_vals_ext_local)
 error_vals_ext[i] = std(time_vals_ext_local)
 time_vals_column[i] = mean(time_vals_column_local)
 error_vals_column[i] = std(time_vals_column_local)
+time_vals_int[i] = mean(time_vals_int_local)
+error_vals_int[i] = std(time_vals_int_local)
 
 end
 
 errorbar(part_choices,time_vals_sing,yerr=[error_vals_sing,error_vals_sing],fmt="-o",label="Single")
 errorbar(part_choices,time_vals_ext,yerr=[error_vals_ext,error_vals_ext],fmt="-o",label="External")
 errorbar(part_choices,time_vals_column,yerr=[error_vals_column,error_vals_column],fmt="-o",label="Column")
+errorbar(part_choices,time_vals_int,yerr=[error_vals_int,error_vals_int],fmt="-o",label="Internal")
 legend()
 xlabel("Particles")
 title("Time for Single Wavefunction Calculation")
